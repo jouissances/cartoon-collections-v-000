f4a644c86867a75e97990fc1e940d0821547dfd9
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 15:47:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |cheese|
-    array.match(cheese_types)
+    cheese_types.include?(cheese)
   }
 end
